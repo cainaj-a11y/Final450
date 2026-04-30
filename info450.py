eea66e74519b8aac5c24882afcf1d567d3d101dc
cainaj-a11y <cainaj@vcu.edu>
2026-04-30 14:12:00 -0400

    Update info450.py

--- info450.py
+++ info450.py
@@ -158,6 +158,7 @@ tradeoff_scatter.update_layout(showlegend=False)
 tradeoff_scatter.update_layout(
     yaxis_title="Number of Employees",
     xaxis=dict(range=[38, 44])
+st.plotly_chart(tradeoff_scatter, use_container_width=True)
 )
 
 """By mapping Average Weekly Hours against Average Annual Income (with bubble size showing workforce volume), this chart reveals the return on time invested across career paths. We can quickly identify:
